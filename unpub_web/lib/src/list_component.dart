9db3762ad4dcae53584f54393cf8e08058ff81c3
Rongjian Zhang <pd4d10@gmail.com>
2019-06-23 20:45:56 +0800

    fix: search result pagination

--- unpub_web/lib/src/list_component.dart
+++ unpub_web/lib/src/list_component.dart
@@ -21,6 +21,7 @@ class ListComponent implements OnInit, OnActivate {
   ListComponent(this.appService);
 
   List<int> get pages {
+    if (data == null) return [];
     var pageCount = (data.count / size).ceil();
     return List.filled(pageCount, 0);
   }
@@ -31,14 +32,19 @@ class ListComponent implements OnInit, OnActivate {
   @override
   void onActivate(RouterState previous, RouterState current) async {
     q = current.queryParameters['q'];
-    currentPage = current.queryParameters['page'] ?? 0;
+    currentPage = int.tryParse(current.queryParameters['page'] ?? '0') ?? 0;
     appService.setLoading(true);
-    data = await appService.fetchPackages(size: size, page: 0, q: q);
+    data = await appService.fetchPackages(size: size, page: currentPage, q: q);
     appService.setLoading(false);
   }
 
-  getListUrl(int page) =>
-      RoutePaths.list.toUrl(parameters: {'q': q, 'page': page.toString()});
+  getListUrl(int page) {
+    Map<String, String> queryParameters = {};
+    if (q != null) queryParameters['q'] = q;
+    if (page > 0) queryParameters['page'] = page.toString();
+
+    return RoutePaths.list.toUrl(queryParameters: queryParameters);
+  }
 
   getDetailUrl(ListApiPackage package) {
     return RoutePaths.detail.toUrl(parameters: {'name': package.name});
