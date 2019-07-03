f074298f5dab4f1063331bf29d8ed74c021d988c
Rongjian Zhang <pd4d10@gmail.com>
2019-07-03 14:46:49 +0800

    fix: dart style format

--- unpub/tool/pre_publish.dart
+++ unpub/tool/pre_publish.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:path/path.dart' as path;
+import 'package:dart_style/dart_style.dart';
 
 var files = ['index.html', 'main.dart.js'];
 
@@ -8,8 +9,8 @@ main(List<String> args) {
     var content =
         File(path.absolute('unpub_web/build', file)).readAsStringSync();
     content = content.replaceAll('\\', '\\\\').replaceAll('\$', '\\\$');
-    content = 'var content ="""$content""";';
+    content = 'var content="""$content""";';
     File(path.absolute('unpub/lib/src/static', file + '.dart'))
-        .writeAsStringSync(content);
+        .writeAsStringSync(DartFormatter().format(content));
   }
 }
